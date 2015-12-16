1359e515a4fb8cac916f8ea8e49b3b30463eae88
Administrator <administrator@dev-mac.mw.local>
2015-12-16 13:57:48 -0400

    Initial Commit

---
+++ WKWebSQL/WKWebSQL.h
@@ -0,0 +1,19 @@
+//
+//  WKWebSQL.h
+//  WKWebSQL
+//
+//  Created by Administrator on 2015-12-16.
+//  Copyright © 2015 Metaworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WKWebSQL.
+FOUNDATION_EXPORT double WKWebSQLVersionNumber;
+
+//! Project version string for WKWebSQL.
+FOUNDATION_EXPORT const unsigned char WKWebSQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WKWebSQL/PublicHeader.h>
+
+
